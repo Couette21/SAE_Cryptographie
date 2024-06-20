51b1d351af7f99ab9953449163c68e3af158e9c0
thorsyss <125518919+thorsyss@users.noreply.github.com>
2024-06-20 23:43:32 +0200

    Delete scripts md5 directory

--- scripts md5/HelloWorld.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/* HelloWorld */
-int main(void) {
-    puts("Bonjour tout le monde !!!");
-    getchar();
-    return 0;
-}
--- scripts md5/crack_hash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Créer un fichier avec les hashes à craquer
-echo "5f4dcc3b5aa765d61d8327deb882cf99" > hashes.txt  # "password" en MD5
-
-# Utiliser hashcat pour craquer le mot de passe
-hashcat -m 0 -a 0 hashes.txt /usr/share/wordlists/rockyou.txt
-
